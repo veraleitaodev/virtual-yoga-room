e0b3da79f902053744079226060254711540c37c
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-04 13:46:29 +0000

    changed background for navbar expanded items

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     
-    
+   // to change background colour to expanded elements on navbar once menu button is clicked
+   $('.navbar-toggler').click(function () {
+    console.log('menu button was clicked, it works!');
+
+    $('#navbarSupportedContent').addClass('bg-black');
+  });
 
 });
