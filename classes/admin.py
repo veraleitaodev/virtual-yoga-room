1075daa22fdf149976c72a93f3e232e81e5d3831
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-05 03:17:45 +0000

    costumised admin

--- classes/admin.py
+++ classes/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Class, Category
 
-# Register your models here.
-admin.site.register(Class)
-admin.site.register(Category)
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'videos',
+        'image',
+        'image_url',
+        'description',
+        'rating'
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Category, CategoryAdmin)
--- classes/models.py
+++ classes/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -15,6 +18,9 @@ class Category(models.Model):
 
 
 class Class(models.Model):
+    class Meta:
+        verbose_name_plural = 'Classes'
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
