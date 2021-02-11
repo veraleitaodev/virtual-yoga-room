f08492f19c56822cb0316be911163edb14a03c81
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-11 00:56:50 +0000

    added sorting to classes in navbar

--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from .models import Class, Program
+from django.shortcuts import render, get_object_or_404
+from .models import Class, Program, Category
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def all_classes(request):
     """ A view to show all classes, including sorting and search queries """
 
     class_list = Class.objects.all()
     page = request.GET.get('page', 1)
+    selected_category = Category.objects.all()
+    selected_program = Program.objects.all()
 
     paginator = Paginator(class_list, 6)
     try:
@@ -18,6 +21,8 @@ def all_classes(request):
 
     context = {
         'classes': classes,
+        'selected_program': selected_program,
+        'selected_category': selected_category,
     }
 
     return render(request, 'products/classes.html', context)
@@ -42,3 +47,29 @@ def all_programs(request):
     }
 
     return render(request, 'products/programs.html', context)
+
+
+def class_detail(request, class_id):
+    """ A view to show individual class details """
+    classes = get_object_or_404(Class, pk=class_id)
+
+    class_list = Class.objects.all()
+    page = request.GET.get('page', 1)
+    selected_category = Category.objects.all()
+    selected_program = Program.objects.all()
+
+    paginator = Paginator(class_list, 6)
+    try:
+        classes = paginator.page(page)
+    except PageNotAnInteger:
+        classes = paginator.page(1)
+    except EmptyPage:
+        classes = paginator.page(paginator.num_pages)
+
+    context = {
+        'classes': classes,
+        'selected_program': selected_program,
+        'selected_category': selected_category,
+    }
+
+    return render(request, 'products/product_detail.html', context)
