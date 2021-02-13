ca44fc9057a9fc71edb106a446d7e7c42066c3b1
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-13 18:24:30 +0000

    updated view in questions app

--- questions/views.py
+++ questions/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
 from .models import Question
 
 
 def faq(request):
     """ A view to return the frequently asked questions page """
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
 
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'questions/faq.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'questions/detail.html', {'question': question})
 
 
 def results(request, question_id):
