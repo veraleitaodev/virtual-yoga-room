985f0e432aaa4e075506b728fa90614ca01032ac
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-14 08:53:39 +0000

    added view to present vote results

--- questions/views.py
+++ questions/views.py
@@ -24,12 +24,15 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    """ A view to present the vote results """
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'questions/results.html', {'question': question})
 
 
 def vote(request, question_id):
     """ A view to present vote form """
+
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
