7a1c0f88c09aabd085ddc1e818694f38eaf76435
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-14 08:41:53 +0000

    added view to vote form

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def faq(request):
@@ -16,6 +16,8 @@ def faq(request):
 
 
 def detail(request, question_id):
+    """ A view to return the the details page where it is viewed the details
+    of a specific question """
 
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'questions/detail.html', {'question': question})
@@ -27,4 +29,16 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    """ A view to present vote form """
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'questions/detail.html',
+                      {'question': question, 'error_message':
+                       "You didn't select a choice.",
+                       })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question_id,)))
