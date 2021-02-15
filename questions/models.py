0dbe1b429fe5c6f80b4cbe7604a96f9405ce44f8
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-15 21:51:37 +0000

    improved choices layout in admin of questions app

--- questions/models.py
+++ questions/models.py
@@ -13,6 +13,8 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
