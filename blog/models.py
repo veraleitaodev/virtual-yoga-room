6ec38885a67eb695e81f81bc212c5a45f281c3c8
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-16 12:19:10 +0000

    added class to blog models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=600)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='media/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self
