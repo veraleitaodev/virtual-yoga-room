2db3d2e420cd1ef87b3cb18f3b38e1b11d313a8a
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-16 08:02:43 +0000

    added filter and search options to admin of questions

--- virtual_yoga_room/settings.py
+++ virtual_yoga_room/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'templates', 'admin'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
