157c410e4c611bc49667571016eb4288bf13be0e
Vera Leitao <veraleitao.dev@gmail.com>
2021-02-22 19:18:25 +0000

    fixed links and views for blog details

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-    path('<int:question_id>/', views.blog_details, name='blog_details'),
+    path('<int:blog_id>/', views.blog_details, name='blog_details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/blog.html', {'blogs': blogs})
 
 
-def blog_details(request):
-    return render(request, 'blog/blog-details.html')
+def blog_details(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blog-details.html', {'blog': blog})
