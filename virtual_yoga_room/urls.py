d482472d9fab0353e46459650a013af314f3da11
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-06 18:56:12 +0000

    added form to blog details template

--- virtual_yoga_room/urls.py
+++ virtual_yoga_room/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('items/', include('items.urls')),
-    path('questions/', include('questions.urls')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
