63e9906ba2cc64df180b24ebc9a71e1e4fe685d3
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-06 19:24:46 +0000

    created add-comment template with view and corrected blog details template

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,18 @@ from .models import Blog
 
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')
-    blog = Blog.objects.all()
-
     return render(request, 'blog/blog.html', {'blogs': blogs})
 
 
 def blog_details(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/blog-details.html', {'blog': blog})
+
+
+def add_comments(request):
+    new_comment = request.POST.get('new_comment')
+    author = request.POST.get('comment_author')
+    Comment.objects.create(new_comment=new_comment, author=author)
+
+    return redirect('blog/blog-details.html')
+    return render(request, 'blog/add-comment.html')
