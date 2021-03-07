d0ba71926119be75e98ff4f3c4e6c6c50166e047
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-07 17:58:52 +0000

    created test for add comment in blog app

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Blog, Comment
 
 
 class TestViews(TestCase):
@@ -8,10 +9,17 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog.html')
 
-    #def test_get_blog(self):
-    
-   # def test_get_comments(self):
+    def test_get_blog(self):
+        blog = Blog.objects.create(
+            title='Test blog post', content='Test content', date='2020-06-10')
+        response = self.client.get(f'/blog/{blog.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog-details.html')
 
-   # def test_can_add_comment(self):
-    
-
+    def test_can_add_comment(self):
+        blog = Blog.objects.create(
+            title='Test blog post', content='Test content', date='2020-06-10')
+        response = self.client.post(f'/blog/{blog.id}/', {
+            'body': 'Test comment body', 'name': 'test', 'active': False})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog-details.html')
