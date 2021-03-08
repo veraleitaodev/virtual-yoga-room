5a18a2eddc16836452540c9c6ae2c1eb4dc39f1b
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-08 22:59:42 +0000

    fixed test bug

--- blog/test_models.py
+++ blog/test_models.py
@@ -5,11 +5,10 @@ from .models import Blog, Comment
 class TestModels(TestCase):
 
     def test_done_defaults_to_false(self):
-        blog = Blog.objects.create(
+        blog1 = Blog.objects.create(
             title='Test blog post', content='Test content', date='2020-06-10')
-        blog_pk = Blog.objects.get(pk=1).pk
         comment = Comment.objects.create(
-            blog=blog_pk, body='test body comment',
+            blog=blog1, body='test body comment',
             name='test', date='2020-06-10')
-        self.assertFalse(blog.author)
+        self.assertFalse(blog1.author)
         self.assertFalse(comment.active)
