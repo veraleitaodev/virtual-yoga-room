06e50831b0c425507ed91f598c58d2b531283a67
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-09 06:09:56 +0000

    fixed url in navbar

--- items/urls.py
+++ items/urls.py
@@ -6,6 +6,7 @@ app_name = 'items'
 urlpatterns = [
     path('classes/', views.all_classes, name='classes'),
     path('programs/', views.all_programs, name='programs'),
-    path('<class_id>', views.class_details, name='class_details'),
-    path('<program_id>', views.program_details, name='program_details'),
+    path('classes/<int:class_id>', views.class_details, name='class_details'),
+    path('programs/<int:program_id>',
+         views.program_details, name='program_details'),
 ]
