2b02560995d3fc8952f42be21a25258d8adfa240
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-10 20:22:13 +0000

    added search views

--- items/views.py
+++ items/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Class, Program
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Class, Program
 
 
 def all_programs(request):
@@ -10,6 +12,7 @@ def all_programs(request):
     page = request.GET.get('page', 1)
 
     paginator = Paginator(programs, 4)
+
     try:
         programs = paginator.page(page)
     except PageNotAnInteger:
@@ -34,6 +37,37 @@ def all_classes(request):
     page = request.GET.get('page', 1)
 
     paginator = Paginator(classes, 6)
+
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                classes = classes.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            classes = classes.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('classes'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            classes = classes.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     try:
         classes = paginator.page(page)
     except PageNotAnInteger:
@@ -44,7 +78,9 @@ def all_classes(request):
     context = {
         'classes': classes,
         'classes_count': classes_count,
-        'page': page
+        'page': page,
+        'search_term': query,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'items/classes.html', context)
