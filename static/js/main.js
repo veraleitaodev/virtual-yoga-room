31646a8b2fe057d6316024cd76d3933cbc3f1ef9
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-13 05:49:48 +0000

    added back to top button

--- static/js/main.js
+++ static/js/main.js
@@ -40,5 +40,22 @@ $(document).ready(function () {
 
             window.location.replace(currentUrl);
         }
-    })
+    });
+    
+    // back to the top button is hidden in default position and shows when the user scrolls down
+    
+    $(window).scroll(function() {
+       console.log("page scrolling!");
+     $('.back-top-btn').show();
+    });
+
+    // when the user clicks on the back to top button, the page returns to the top and the button disappears
+    $('.back-top-link').click(function () {
+        console.log("button was clicked!"); 
+         
+        window.scrollTo(0,0);
+        $('.back-top-btn').hide();
+        location.reload(true)
+        
+    });
 });
