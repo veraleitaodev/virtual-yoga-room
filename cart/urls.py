36feb0fb3e01cb92487c776a1b420013b0b59730
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-13 08:36:34 +0000

    added continuing shopping to the shopping bag page

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.view_cart, name='view_cart')
+]
--- virtual_yoga_room/urls.py
+++ virtual_yoga_room/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('items/', include('items.urls')),
     path('blog/', include('blog.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
