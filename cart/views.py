00bdbde6341b2eba4b6219508878ee7c22102088
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-13 14:28:33 +0000

    created add_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from items.models import Program, Lecture
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +9,26 @@ def view_cart(request):
     """A view that renders the cart contents page"""
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add the item to the cart to the cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    program = Program.objects.get(pk=item_id)
+    lecture = Lecture.objects.get(pk=item_id)
+    cart = request.session.get('cart', {})
+    if request.POST:
+        if item_id in list(cart.keys()):
+            messages.error(
+                request,
+                'Item already in your cart!\
+                Check your cart to continue purchase.')
+            return redirect(redirect_url)
+        else:
+            cart[item_id] = quantity
+            messages.success(request, 'Item was added to your cart')
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
