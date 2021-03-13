0411702ed32697c6e9b6d81ffede76bc49274b97
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-13 15:02:27 +0000

    recovered .gitignore file and added view to cart app

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect
 from items.models import Program, Lecture
 from django.contrib import messages
 
