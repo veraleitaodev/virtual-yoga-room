c6700b4f96f7e7184554007a924431e3e1bb15c2
Vera Leitao <veraleitao.dev@gmail.com>
2021-03-17 07:39:22 +0000

    fixed contexts.py functionality

--- virtual_yoga_room/settings.py
+++ virtual_yoga_room/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.contexts.car_contents',
+                'cart.contexts.cart_contents',
             ],
         },
     },
